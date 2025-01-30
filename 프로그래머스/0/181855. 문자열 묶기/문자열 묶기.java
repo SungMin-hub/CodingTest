0fae0b0566a24739d2469a4b5a11ae76aa7aea54
SungMin-hub <sungmin806@gmail.com>
2025-01-30 19:24:52 +0900

    [level 0] Title: 문자열 묶기, Time: 7.19 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        
+        int[] arr = new int[31];
+        
+        for(int i=0; i<strArr.length; i++) arr[strArr[i].length()]++;
+        
+        Arrays.sort(arr);
+        
+        return arr[30];
+    }
+}
