89ad79ed6ef1274f12564a5edc6a220601e4c945
SungMin-hub <sungmin806@gmail.com>
2025-02-07 20:09:53 +0900

    [level 0] Title: 외계어 사전, Time: 2.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        String str = "";
+        
+        Arrays.sort(spell);
+        for(int i=0; i<spell.length; i++) str += spell[i];
+        
+        for(int i=0; i<dic.length; i++){
+            char[] dic_spell = dic[i].toCharArray();
+            Arrays.sort(dic_spell);
+            String result = new String(dic_spell);
+            if(str.equals(result)) answer = 1;
+        }
+        
+        return answer;
+    }
+}
