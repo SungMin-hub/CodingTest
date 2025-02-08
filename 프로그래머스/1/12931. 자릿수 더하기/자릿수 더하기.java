edd1f0ece76c4d04a0b2374ee6c1a5b945c73ab1
SungMin-hub <sungmin806@gmail.com>
2025-02-08 09:17:41 +0900

    [level 1] Title: 자릿수 더하기, Time: 7.95 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = n+"";
+        for(int i=0; i<str.length(); i++) answer += str.charAt(i) - '0';
+        System.out.println(answer);
+
+        return answer;
+    }
+}
