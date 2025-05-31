6abfb16f2354a3c7a4066ba4c1449eeac74d803f
SungMin-hub <sungmin806@gmail.com>
2025-05-31 22:58:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.83 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(int i=0; i<participant.length; i++){
+            map.put(participant[i],map.getOrDefault(participant[i],0)+1);
+        }
+        
+        for(String com : completion){
+            map.put(com,map.getOrDefault(com,0)-1);
+        }
+        
+        for(String key : map.keySet()){
+            if(map.get(key) > 0) return key;
+        }
+        
+        return null;
+    }
+}
