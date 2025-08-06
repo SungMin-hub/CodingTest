7525f50638e321ec6f5ff308c7919e87894bd201
SungMin-hub <sungmin806@gmail.com>
2025-08-06 13:41:44 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25712 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(
+			(a,b) -> {
+				int A = Math.abs(a);
+				int B = Math.abs(b);
+				if (A == B) return a-b;
+				return A - B;
+			});
+		StringBuilder sb = new StringBuilder();
+
+		for(int i=0;i<N;i++){
+			int x = Integer.parseInt(br.readLine());
+			if(x != 0){
+				pq.add(x);
+			} else{
+				if(pq.isEmpty()) sb.append(0).append("\n");
+				else sb.append(pq.poll()).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
