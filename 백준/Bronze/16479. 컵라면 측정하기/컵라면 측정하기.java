326fa5770a44754c13e12890b48473cad42de190
SungMin-hub <sungmin806@gmail.com>
2025-10-04 23:43:03 +0900

    [Bronze III] Title: 컵라면 측정하기, Time: 108 ms, Memory: 14220 KB, Score: 40 point -BaekjoonHub

--- 백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.java
+++ 백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.java
@@ -6,11 +6,12 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long K = Long.parseLong(br.readLine());
+		double K = Double.parseDouble(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		long D1 = Long.parseLong(st.nextToken());
-		long D2 = Long.parseLong(st.nextToken());
+		double D1 = Double.parseDouble(st.nextToken());
+		double D2 = Double.parseDouble(st.nextToken());
 
-		System.out.println(K * K - (D1 - D2) * (D1 - D2) / 4);
+		double ans = K * K - Math.pow((D1 - D2) / 2.0, 2);
+		System.out.println(ans);
 	}
 }
