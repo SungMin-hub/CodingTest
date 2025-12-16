cc779513379b47c7351d4b7b3376c42de17aa245
SungMin-hub <sungmin806@gmail.com>
2025-12-16 23:41:52 +0900

    [Bronze I] Title: 테니스 시합, Time: 112 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2183. 테니스 시합/테니스 시합.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		String winner = st.nextToken();
+
+		char ans = winner.charAt(winner.length() - 1);
+		System.out.println(ans);
+	}
+}
