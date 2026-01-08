53674753817a042292659efca5206111cb26ef70
SungMin-hub <sungmin806@gmail.com>
2026-01-08 19:33:48 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		int ans = Math.max(A + C, P);
+		System.out.print(ans);
+	}
+}
