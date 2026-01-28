0d9a062293b0888f87b1347c1f8db05c91f60cfb
SungMin-hub <sungmin806@gmail.com>
2026-01-28 22:12:18 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 356 ms, Memory: 21344 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		for(int i = 1; i <= 2*N; i++){
+
+			if(i % 2 == 1){
+				for(int j = 1; j <= N; j++){
+					if(j % 2 == 1){
+						System.out.print("*");
+					}
+					else{
+						System.out.print(" ");
+					}
+				}
+			}
+
+			else{
+				for(int j = 1; j <= N; j++){
+					if(j % 2 == 1){
+						System.out.print(" ");
+					}
+					else{
+						System.out.print("*");
+					}
+				}
+			}
+			System.out.print("\n");
+
+		}
+	}
+}
