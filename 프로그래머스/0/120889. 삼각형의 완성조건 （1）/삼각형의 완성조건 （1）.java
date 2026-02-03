145e91199115bb875622ff48fcf7ad9f405f46da
SungMin-hub <sungmin806@gmail.com>
2026-02-03 21:31:08 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        int max =0;
+        int sum =0;
+        
+        for(int i =0; i<sides.length; i++){
+            if(sides[i] > max){
+                max = sides[i];
+            }
+            sum += sides[i];
+        }
+        
+        if(sum - max <= max) answer = 2;
+        else answer = 1;
+        
+        return answer;
+    }
+}
