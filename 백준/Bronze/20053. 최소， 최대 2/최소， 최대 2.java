efd91483734b4319960c9ee8df69555c1d815553
SungMin-hub <sungmin806@gmail.com>
2026-02-20 17:05:12 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 832 ms, Memory: 250380 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while(T-- > 0){
+            int N = Integer.parseInt(br.readLine());
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int i=0; i<N; i++){
+                int num = Integer.parseInt(st.nextToken());
+                max = Math.max(max,num);
+                min = Math.min(min,num);
+            }
+            sb.append(min).append(' ').append(max).append("\n");
+        }
+
+        System.out.println(sb);
+	}
+}
