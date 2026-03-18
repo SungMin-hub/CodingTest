c8113e5caaa0c9b1f600827905df6c445ba42792
SungMin-hub <sungmin806@gmail.com>
2026-03-18 23:31:46 +0900

    [Bronze V] Title: 공통교육과정, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        char str = input.charAt(0);
+
+        switch (str) {
+            case 'F':
+                System.out.println("Foundation");
+                break;
+            case 'C':
+                System.out.println("Claves");
+                break;
+            case 'V':
+                System.out.println("Veritas");
+                break;
+            case 'E':
+                System.out.println("Exploration");
+                break;
+        }
+    }
+}
